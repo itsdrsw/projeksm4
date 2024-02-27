4cdc4ff99c6b690ee690a8d367b6b89e8066364f
itsdrsw <marswan27@gmail.com>
2024-02-27 12:14:34 +0700

    upload frontend & backend
    Mengunggah home frontend dan dashboard backend menggunakan template sesuai BKPM

--- WEB/routes/web.php
+++ WEB/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['namemspaace' => 'Frontend'], function() {
+    Route::resource('home', HomeController::class);
+});
+
+Route::group(['namemspaace' => 'Backend'], function() {
+    Route::resource('dashboard', DashboardController::class);
 });
